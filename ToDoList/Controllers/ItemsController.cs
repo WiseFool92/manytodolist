6e2d40b3d92e5b702f5a51ae6980df1daa6dfc2b
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-26 15:15:53 -0700

    replace verbose code, for entity refactoring

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,34 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-    [HttpGet("/categories/{categoryId}/items/new")] // The path now includes the ID of the Category we're adding a new Item to. Because it's in curly braces, we can grab this in our route's parameter to locate the Category object and pass it into the corresponding view
-    public ActionResult New(int categoryId)
+    private readonly ToDoListContext _db;
+
+    public ItemsController(ToDoListContext db)
     {
-      Category category = Category.Find(categoryId);
-      return View(category);
+      _db = db;
     }
 
-    [HttpGet("/categories/{categoryId}/items/{itemId}")] // The path now includes Category information, which ensures our routes are now RESTfully named.
-    public ActionResult Show(int categoryId, int itemId) // Because the path includes both Item and Category IDs, we can locate the correct parent and child objects and pass them to our view in a Dictionary.
+    public ActionResult Index()
     {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("item", item);
-      model.Add("category", category);
+      List<Item> model = _db.Items.ToList();
       return View(model);
-    }
-
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
-    {
-      Item.ClearAll();
-      return View();
     }
   }
 }
