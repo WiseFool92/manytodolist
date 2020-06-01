5b3827a2a14c51e602f6d631017ffcf39d7d44f0
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-06-01 10:25:29 -0700

    update Models w/CategoryItem to join w/entity

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,10 +5,26 @@ namespace ToDoList.Models
 {
   public class Item
   {
+    public Item()
+    {
+      this.Categories = new HashSet<CategoryItem>();
+    }
     public int ItemId { get; set; }
     public string Description { get; set; }
 
+    public ICollection<CategoryItem> Categories { get; }
+
+    // Code below may be superfluous extra
     public int CategoryId { get; set; }
     public virtual Category Category { get; set; }
+  }
+
+  public class CategoryItem
+  {
+    public int CategoryItemId { get; set; }
+    public int ItemId { get; set; }
+    public int CategoryId { get; set; }
+    public Item Item { get; set; }
+    public Category Category { get; set; }
   }
 }
