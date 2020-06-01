0aeceafd2339ba31be238c20c99fedca1bfd952c
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-06-01 15:21:26 -0700

    comment out extra code

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -14,17 +14,17 @@ namespace ToDoList.Models
 
     public ICollection<CategoryItem> Categories { get; }
 
-    // Code below may be superfluous extra
-    public int CategoryId { get; set; }
-    public virtual Category Category { get; set; }
-  }
+    public class CategoryItem
+    {
+      public int CategoryItemId { get; set; }
+      public int ItemId { get; set; }
+      public int CategoryId { get; set; }
+      public Item Item { get; set; }
+      public Category Category { get; set; }
+    }
 
-  public class CategoryItem
-  {
-    public int CategoryItemId { get; set; }
-    public int ItemId { get; set; }
-    public int CategoryId { get; set; }
-    public Item Item { get; set; }
-    public Category Category { get; set; }
+    // Code below may be superfluous extra
+    // public int CategoryId { get; set; }
+    // public virtual Category Category { get; set; }
   }
 }
